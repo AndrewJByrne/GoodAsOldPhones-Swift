4b0965d6e6fd08a0a9d0c96bd5d65cdc64adf5d1
AndrewJByrne <anbyrne@microsoft.com>
2016-03-05 16:21:34 -0800

    Soup to Bits Additions - added a Tab Bar View Control so we can see and navigate through all three screens. Add icons and labels to tab bar.

---
+++ GoodAsOldPhones/CartTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CartTableViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Andrew Byrne on 3/5/16.
+//  Copyright © 2016 Andrew Byrne. All rights reserved.
+//
+
+import UIKit
+
+class CartTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+
+       // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+}
---
+++ SoupToBits-notes.md
@@ -0,0 +1,9 @@
+# Notes about [Soup to Bits: App Evolution With Swift](https://www.codeschool.com/screencasts/soup-to-bits-app-evolution-with-swift)
+
+* Tab Bar Controller
+* Relationship Segue
+* Build and Run = CMD + R
+
+
+ New icons for tab bar found on [GitHub](https://github.com/codeschool/AppEvolutionWithSwiftDemo/tree/master/Image%20Files)
+
